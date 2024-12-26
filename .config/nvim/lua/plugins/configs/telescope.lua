b2ebcb981064aa81702e9ffa1d26cbe6ee63b602
Jojo <joachim.azzi@gmail.com>
2024-12-26 12:08:38 +0100

    adding rocks

--- .config/nvim/lua/plugins/configs/telescope.lua
+++ .config/nvim/lua/plugins/configs/telescope.lua
@@ -1,5 +1,4 @@
 local telescope = require("telescope")
-local commander = require("commander")
 
 local exec = function(s)
     return "<cmd>" .. s .. "<cr>"
@@ -21,31 +20,9 @@ local opts = {
 telescope.setup(opts)
 
 local exts = {
-    "dap",
-    "switch",
-    "project",
-    "file_browser",
-    "frecency",
-    "picker_list",
     "cmdline",
-    "oil"
 }
 
 for _, ext in pairs(exts) do
     telescope.load_extension(ext)
 end
-
-commander.add({
-    {
-        desc = "Close Buffer",
-        cmd = "<cmd>bw<cr>",
-    },
-    {
-        desc = "Telescope (All Pickers)",
-        cmd = "<cmd>Telescope picker_list<cr>",
-    },
-    {
-        desc = "Run Task",
-        cmd = exec("OverseerRun"),
-    }
-})
